3734583d4082ad59c0227cf3373e5e71f2cf62c3
Ririn Afifah <syafrin04011962@gmail.com>
2025-05-28 17:59:44 +0700

    script

--- json/script.js
+++ json/script.js
@@ -1,22 +1,7 @@
-// let mahasiswa = {
-//     nama : "Ririn Afifah",
-//     nim : "2217020031",
-//     email : "ririnafifah021@gmail.com"
-// }
+let mahasiswa = {
+    nama : "Ririn Afifah",
+    nim : "2217020031",
+    email : "ririnafifah021@gmail.com"
+}
 
-// console.log(JSON.stringify(mahasiswa));
-
-// let xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function () {
-//     if(xhr.readyState == 4 && xhr.status == 200){
-//         let mahasiswa = JSON.parse(this.responseText);
-//         console.log(mahasiswa);
-//     }
-// }
-// xhr.open('GET', 'coba.json', true);
-// xhr.send();
-
-$.getJSON('coba.json', function (data) {
-    console.log(data);
-
-});
+console.log(mahasiswa);
