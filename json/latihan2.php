1c4bea911cf50228a7a9e343fb8f3086fd97c2af
Ririn Afifah <syafrin04011962@gmail.com>
2025-05-28 17:00:34 +0700

    true

--- json/latihan2.php
+++ json/latihan2.php
@@ -1,7 +1,7 @@
 <?php
 
 $data = file_get_contents('coba.json');
-$mahasiswa = json_decode($data, true);
+$mahasiswa = json_decode($data, false);
 
 var_dump($mahasiswa);
 echo $mahasiswa[0] ["pembimbing"]["pembimbing1"];
