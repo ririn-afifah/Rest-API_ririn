8222e5924e87a4d5b82f5fa5016c7002ade5d2d6
Ririn Afifah <syafrin04011962@gmail.com>
2025-05-29 16:26:09 +0700

    menambahkan gambar

--- wpu_portofolio/index.php
+++ wpu_portofolio/index.php
@@ -71,183 +71,133 @@
     </section>
 
     <!-- Youtube & IG -->
-     <section class="social bg-light" id="social">
-      <div class="container">
-        <div class="row pt-4 mb-4">
-          <div class="col text-center">
-            <h2>Social Media</h2>
+     <!-- Youtube & IG -->
+<section class="social bg-light" id="social">
+  <div class="container">
+    <div class="row pt-4 mb-4">
+      <div class="col text-center">
+        <h2>Social Media</h2>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <!-- YouTube -->
+      <div class="col-md-5">
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <img src="img/profile1.png" width="200" class="rounded-circle img-thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h5>WebProgrammingUIN</h5>
+            <p>1000 Subscriber.</p>
           </div>
         </div>
-
-        <div class="row justify-content-center">
-          <div class="col-md-5">
-            <div class="row">
-              <div class="col-md-4">
-                <img src="img/profile1.png" width="200" class="rounded-circle img-thumbnail">
-              </div>
-              <div class="col-md-8">
-                <h5>WebProgrammingUIN</h5>
-                <p>Subscriber 10000</p>
-              </div>
-            </div>
-            <div class="row mt-3 pb-3">
-              <div class="col">
-              <div class="embed-responsive embed-responsive-16by9">
+        <div class="row">
+          <div class="col">
+            <div class="embed-responsive embed-responsive-16by9">
               <iframe class="embed-responsive-item" 
-              src="https://www.youtube.com/embed/wqx47Z129d4?rel=0" 
-              allowfullscreen></iframe>
-              </div>
-              </div>
-            </div>
-            </div>
-            <div class="col-md-8">
-              <h5>@cookiey_daughh</h5>
-              <p>202 Followers</p>
+                      src="https://www.youtube.com/embed/wqx47Z129d4?rel=0" 
+                      allowfullscreen></iframe>
             </div>
           </div>
-            
-          <div class="row mt-3 pb-3">
-            <div class="col">
-              <div class="ig-thumbnail">
-                <img src="img/thumbs/1.png">
-              </div>
-              <div class="ig-thumbnail">
-                <img src="img/thumbs/2.png">
-              </div>
-              <div class="ig-thumbnail">
-                <img src="img/thumbs/3.png">
-              </div>
+        </div>
+      </div>
+
+      <!-- Instagram -->
+      <div class="col-md-5">
+        <div class="row mb-3">
+          <div class="col-md-4">
+            <img src="img/profile1.png" width="200" class="rounded-circle img-thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h5>@cookiey_daughh</h5>
+            <p>202 Followers.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col d-flex justify-content-between">
+            <div class="ig-thumbnail">
+              <img src="img/thumbs/1.png" width="100">
+            </div>
+            <div class="ig-thumbnail">
+              <img src="img/thumbs/2.png" width="100">
+            </div>
+            <div class="ig-thumbnail">
+              <img src="img/thumbs/3.png" width="100">
             </div>
           </div>
-       </div> 
-  </section>
-
-
+        </div>
+      </div>
+    </div>
+  </div>
 
     <!-- Portfolio -->
-    <section class="portfolio bg-dark" id="portfolio">
-      <div class="container">
-        <div class="row pt-4 mb-4">
-          <div class="col text-center">
-            <h2>Portfolio</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/1.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/3.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>   
-        </div>
-
-        <div class="row">
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/4.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div> 
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/5.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/6.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
+    <!-- Portfolio -->
+<section class="portfolio bg-dark" id="portfolio">
+  <div class="container">
+    <div class="row pt-4 mb-4">
+      <div class="col text-center">
+        <h2 class="text-light">Portfolio</h2> <!-- Tambahkan class text-light -->
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md mb-4">
+        <div class="card">
+          <img class="card-img-top" src="img/thumbs/1.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
         </div>
       </div>
-    </section>
 
-
-    <!-- Contact -->
-    <section class="contact bg-light" id="contact">
-      <div class="container">
-        <div class="row pt-4 mb-4">
-          <div class="col text-center">
-            <h2>Contact</h2>
-          </div>
-        </div>
-
-        <div class="row justify-content-center">
-          <div class="col-lg-4">
-            <div class="card bg-primary text-white mb-4 text-center">
-              <div class="card-body">
-                <h5 class="card-title">Contact Me</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-            
-            <ul class="list-group mb-4">
-              <li class="list-group-item"><h3>Location</h3></li>
-              <li class="list-group-item">My Office</li>
-              <li class="list-group-item">Jl. Setiabudhi No. 193, Bandung</li>
-              <li class="list-group-item">West Java, Indonesia</li>
-            </ul>
-          </div>
-
-          <div class="col-lg-6">
-            
-            <form>
-              <div class="form-group">
-                <label for="nama">Nama</label>
-                <input type="text" class="form-control" id="nama">
-              </div>
-              <div class="form-group">
-                <label for="email">Email</label>
-                <input type="text" class="form-control" id="email">
-              </div>
-              <div class="form-group">
-                <label for="phone">Phone Number</label>
-                <input type="text" class="form-control" id="phone">
-              </div>
-              <div class="form-group">
-                <label for="message">Message</label>
-                <textarea class="form-control" id="message" rows="3"></textarea>
-              </div>
-              <div class="form-group">
-                <button type="button" class="btn btn-primary">Send Message</button>
-              </div>
-            </form>
-
+      <div class="col-md mb-4">
+        <div class="card">
+          <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
         </div>
       </div>
-    </section>
 
+      <div class="col-md mb-4">
+        <div class="card">
+          <img class="card-img-top" src="img/thumbs/3.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>   
+    </div>
+
+    <div class="row">
+      <div class="col-md mb-4">
+        <div class="card">
+          <img class="card-img-top" src="img/thumbs/4.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div> 
+      <div class="col-md mb-4">
+        <div class="card">
+          <img class="card-img-top" src="img/thumbs/5.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md mb-4">
+        <div class="card">
+          <img class="card-img-top" src="img/thumbs/6.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
     <!-- footer -->
     <footer class="bg-dark text-white mt-5">
