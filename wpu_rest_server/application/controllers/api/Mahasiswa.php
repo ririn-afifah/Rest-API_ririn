950234de931cafccc4b1b891e5983674c0540014
Ririn Afifah <syafrin04011962@gmail.com>
2025-05-31 02:49:54 +0700

    mengembalikan data mahasiswa

---
+++ wpu_rest_server/application/controllers/api/Mahasiswa.php
@@ -0,0 +1,33 @@
+<?php 
+use Restserver\Libraries\REST_Controller;
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Mahasiswa extends REST_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+    }
+
+    public function index_get()
+    {
+        $mahasiswa = $this->Mahasiswa_model->getMahasiswa();
+
+        if ($mahasiswa) {
+            $this->response([
+                'status' => true,
+                'data' => $mahasiswa
+            ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Data tidak ditemukan'
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+}
